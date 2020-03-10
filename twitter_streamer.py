f9b70c3792215cceecc4fef6367bfd992b820fe6
DinJakupi <din.jakupi@gmail.com>
2020-03-10 15:56:34 +0100

    Twitter API connection & authentication and getting new tweets of Donald Trump

--- twitter_streamer.py
+++ twitter_streamer.py
@@ -2,6 +2,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 
+import time
 import twitter_credentials
 
 AUTH = OAuthHandler(twitter_credentials.CONSUMER_TOKEN, twitter_credentials.CONSUMER_SECRET)
@@ -13,7 +14,7 @@ class TwitterStreamer():
     def stream_tweets(self, filename):
         #handles authentication and connection with Twitter API
 
-        listener = StdOutListener(filename) 
+        listener = StdOutListener(filename, time_limit=20) 
 
         #create stream and filter to user @realDonaldTrump
         stream = Stream(AUTH, listener)
@@ -26,14 +27,21 @@ class TwitterStreamer():
 
 class StdOutListener(StreamListener):
     #class to process received tweets
-
-    def __init__(self, filename):
+    def __init__(self, filename, time_limit=60):
        self.filename = filename
+       self.start_time = time.time()
+       self.limit = time_limit
+
+
     def on_data(self, data):
         try:
-            with open(self.filename, 'a') as tf:
+            tf = open(filename, 'a')
+            if(time.time() - self.start_time) < self.limit:
                 tf.write(data)
-            return True
+                return True
+            else:
+                tf.close()
+                return False
         except BaseException as e:
             print("Error on_data %s" % str(e))
         return True
@@ -41,10 +49,12 @@ class StdOutListener(StreamListener):
     def on_error(self, status):
         print(status)
 
-
+class JsonParser(self):
+    
+    def
 if __name__ == "__main__":
 
-    filename  = "tweets.txt"
+    filename  = "tweets.json"
     twitter_streamer = TwitterStreamer()
     twitter_streamer.stream_tweets(filename)
     #twitter_streamer.post_tweets()
