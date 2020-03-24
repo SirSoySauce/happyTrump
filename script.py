02fe9b6fa845aea0790739728855cfd5e2fe33e7
Ivo Cavelti <ivo.cavelti@bluewin.ch>
2020-03-24 09:33:17 +0100

    Added functionality to fetch and tweet tweets

--- script.py
+++ script.py
@@ -1,4 +1,6 @@
 import re
+import tweepy
+import twitter_credentials
 
 
 def get_word_dict_from_file(file):
@@ -17,7 +19,26 @@ def replace_words_from_dict(dict, string):
 
 
 word_file = open('words.txt', 'r')
-
 word_dict = get_word_dict_from_file(word_file)
 
-print(replace_words_from_dict(word_dict, "Let me get this straight: Democrats and the mainstream media are trying to incite panic over the Coronavirus ...Yet they still push an open border agenda that allows thousands of unchecked & potentially unhealthy migrants to flow into America?"))
+auth = tweepy.OAuthHandler(twitter_credentials.CONSUMER_TOKEN, twitter_credentials.CONSUMER_SECRET)
+auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+f = open("latest_tweet_id.txt", "r")
+latest_tweet_id = f.read()
+
+timeline = api.user_timeline(user_id="25073877", since_id=latest_tweet_id, tweet_mode="extended")
+
+for tweet in timeline:
+    firstText = tweet.full_text
+    editedText = replace_words_from_dict(word_dict, firstText)
+    if firstText != editedText:
+        print(firstText)
+        api.update_status(status=editedText)
+
+if timeline.__len__() > 0:
+    file_write = open("latest_tweet_id.txt", "w")
+    file_write.write(timeline[0].id_str)
+f.close()
